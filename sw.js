fc53cb091d2e000670335dfbdc6430b0274c6859
themortgagemaster01-eng <themortgagemaster01@gmail.com>
2026-07-03 12:03:42 -0400

    Redesign UI: cosmic navy + champagne gold, art-deco glassmorphism (functionality preserved)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chronovisor-v3';
+const CACHE = 'chronovisor-v4';
 const SHELL = ['.', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
